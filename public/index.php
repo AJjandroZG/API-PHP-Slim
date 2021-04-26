fecaa58106b0115f690dbe914cafac2469d53c3e
Alejandro Zelaya <azelaya@yourappland.com>
2021-04-26 15:09:10 -0600

    docs: ✏️ update

--- public/index.php
+++ public/index.php
@@ -2,11 +2,6 @@
 // Librarie
 require __DIR__ . '/../vendor/autoload.php';
 // Load ENV
-$dotenv = new Dotenv\Dotenv();
-if(getenv('APP_ENV') === 'development') {
-    $dotenv->load(__DIR__);
-}
-$dotenv->required('OTHER_VAR');
 // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 // $dotenv->load();
 // Project
