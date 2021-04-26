6c946410f808ce16393094673075f1d6f8d47483
Alejandro Zelaya <azelaya@yourappland.com>
2021-04-26 14:33:58 -0600

    docs: ✏️ view errors

--- src/routes/v1.php
+++ src/routes/v1.php
@@ -8,7 +8,7 @@ $version = "/v1";
 // Instantiate App
 $app = AppFactory::create();
 // Add error middleware
-$app->addErrorMiddleware(false, true, true);
+$app->addErrorMiddleware(true, true, true);
 // allow CORS
 
 $app->add(function ($request, $handler) {
